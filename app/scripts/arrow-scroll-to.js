d751651680e77b435911b3f82ded50a19a761c8e
Yann Bertrand <yann@bertrand.com>
2015-11-28 22:33:32 +0100

    Add social icons

--- app/scripts/arrow-scroll-to.js
+++ app/scripts/arrow-scroll-to.js
@@ -2,11 +2,13 @@
  * arrow scroll to effect by ZeZeN
  * benavern.github.io
  */
+/*global $*/
+'use strict';
 
-$('a[data-scroll-to]').on('click', function(e) { // on click
+$('a[data-scroll-to]').on('click', function (e) { // on click
   e.preventDefault();
   var page = $(this).attr('data-scroll-to'); // target
   var speed = 750; // animation duration (ms)
-  $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+  $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
   return false;
 });
