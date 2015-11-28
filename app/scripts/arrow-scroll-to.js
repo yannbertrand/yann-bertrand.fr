c8e365fce64c67e7fcac7b813967a2e68a69ce59
Yann Bertrand <yann@bertrand.com>
2015-11-28 21:57:14 +0100

    Page preparation

---
+++ app/scripts/arrow-scroll-to.js
@@ -0,0 +1,12 @@
+/**
+ * arrow scroll to effect by ZeZeN
+ * benavern.github.io
+ */
+
+$('a[data-scroll-to]').on('click', function(e) { // on click
+  e.preventDefault();
+  var page = $(this).attr('data-scroll-to'); // target
+  var speed = 750; // animation duration (ms)
+  $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+  return false;
+});
